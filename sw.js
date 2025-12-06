81ae496fb9d09e32791dda13da3bec8d4c758e9f
serusoft <serusoft4@gmail.com>
2025-12-06 07:02:48 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
+// Service Worker for Skore Point
 const CACHE_NAME = 'skore-point-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  // Add other static assets you want to cache
+  'skore-icon.jpg'
 ];
 
+// Install event
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -14,25 +16,24 @@ self.addEventListener('install', event => {
   );
 });
 
+// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
       })
   );
 });
 
+// Activate event
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
